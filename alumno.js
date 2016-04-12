d3dbfd53d0d665bd110e9a090bd7cb671d6c3cef
Nicolas Kovalsky <kovalsky.nicolas@gmail.com>
2016-04-12 20:01:10 -0300

    agrego al alumno

--- alumno.js
+++ alumno.js
@@ -1,9 +1,46 @@
 var app = require('express')();
+var http = require('http');
+var io = require('socket.io-client');;
 var bodyParser = require('body-parser')
+
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
+
+
+var options = {
+	host: 'http://localhost',
+    port: 3000,
+    path: '/question',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
+
+
+var availableQuestions = [ { question:'que hora es?', emitter:'kova'}, { question: 'que dia es hoy?', emitter:'tomi'}, {question:'Quien soy?', emmiter:'Alan'}];
+
+//SyncedQuestionsOfList
+var questionsList = {};
+
+var socket = io(options.host);
+
+socket.on('questions', function (data) {
+ 	questionsList = data;
+ 	console.log(data);
+});
+
+
+setInterval(function(){
+
+socket.emit('question', availableQuestions[Math.floor(Math.rand() * 3 )]);
+
+}, 1000); 
+
+
 
 
 
